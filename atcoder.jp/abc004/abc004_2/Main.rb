f46311dd22d1ebd0612e3b9f977504e97521a331
tomcha <tomcha@tomcha.net>
2014-02-21 18:42:37 +0900

    [AC] abc004 abc004_2

---
+++ atcoder.jp/abc004/abc004_2/Main.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+first_line = gets.chomp.split('')
+second_line = gets.chomp.split('')
+third_line= gets.chomp.split('')
+force_line = gets.chomp.split('')
+
+puts force_line.reverse.join('')
+puts third_line.reverse.join('')
+puts second_line.reverse.join('')
+puts first_line.reverse.join('')
