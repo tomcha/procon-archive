415df779e0207a583d57f80a027f1e1dc2fb277e
tomcha <tomcha@tomcha.net>
2015-04-01 18:24:49 +0900

    [AC] abc001 abc001_1

---
+++ atcoder.jp/abc001/abc001_1/Main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  int before, after;
+  int answer;
+  scanf("%d", &after);
+  scanf("%d", &before);
+
+  answer = after - before;
+
+  printf("%d\n", answer);
+  return 0;
+}
